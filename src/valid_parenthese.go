49286c894ce0e839b9cdab897dbd140a54ccf45a
binnguci <21130291@st.hcmuaf.edu.vn>
2025-03-03 14:54:41 +0700

    feat: implement the second exercise

---
+++ src/valid_parenthese.go
@@ -0,0 +1,24 @@
+package main
+
+// https://leetcode.com/problems/valid-parentheses/
+// Bài 2
+func isValid(s string) bool {
+    stack := []rune{}
+    brackets := map[rune]rune{')': '(', '}': '{', ']': '['}
+
+    for _, char := range s {
+        if char == '(' || char == '{' || char == '[' {
+            stack = append(stack, char)
+        } else {
+            if len(stack) == 0 {
+                return false
+            }
+            top := stack[len(stack)-1] 
+            stack = stack[:len(stack)-1]
+            if brackets[char] != top {
+                return false
+            }
+        }
+    }
+    return len(stack) == 0
+}
