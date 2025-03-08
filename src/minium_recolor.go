35d3b2fb771a97fadee41525880af460164ca620
binnguci <21130291@st.hcmuaf.edu.vn>
2025-03-08 15:21:18 +0700

    feat: implement ex 2379

---
+++ src/minium_recolor.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//Bài 2379
+//https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/?envType=daily-question&envId=2025-03-08
+
+func minimumRecolors(blocks string, k int) int {
+    blackCount, ans := 0, k
+    
+    for i := 0; i < len(blocks); i++ {
+        if i >= k && blocks[i-k] == 'B' {
+            blackCount--
+        }
+        if blocks[i] == 'B' { 
+            blackCount++
+        }
+        if ans > k-blackCount {
+            ans = k-blackCount
+        }
+    }
+    
+    return ans
+}
+
+func main() {
+	fmt.Println(minimumRecolors("WBBWWBBWBW", 7))
+}
