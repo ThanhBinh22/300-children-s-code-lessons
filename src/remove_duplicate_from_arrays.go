d79f1e82baeb810dc67919749a6ecfe96da50a64
binnguci <21130291@st.hcmuaf.edu.vn>
2025-03-08 20:00:02 +0700

    feat: implement 26th exercise

---
+++ src/remove_duplicate_from_arrays.go
@@ -0,0 +1,17 @@
+package main
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+//Bài 26
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	writeIndex := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[writeIndex] = nums[i]
+			writeIndex++
+		}
+	}
+	return writeIndex
+}
